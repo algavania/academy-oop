93d0df19704b8b738851b2ff1702ac9033ed4749
Alga <algavania454@gmail.com>
2023-11-17 11:34:57 +0700

    feat: [AV] prepare category model and repository

---
+++ src/main/java/com/oop/academy/models/Category.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.oop.academy.models;
+
+/**
+ *
+ * @author MSI
+ */
+public class Category {
+    private String name;
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
