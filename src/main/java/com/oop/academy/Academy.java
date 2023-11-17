2ea81f4fe85ace56c45678c10d0c9b2cc9f14d29
Alga <algavania454@gmail.com>
2023-11-17 11:17:13 +0700

    chore: [AV] init project

---
+++ src/main/java/com/oop/academy/Academy.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.oop.academy;
+
+/**
+ *
+ * @author MSI
+ */
+public class Academy {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
