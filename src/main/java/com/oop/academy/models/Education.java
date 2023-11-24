b8a96dc208f685f325ae6ee0f5b05a484cfc24c8
Ezra <ezra3sh@gmail.com>
2023-11-24 09:22:27 +0700

    feat: [ES] update CourseContent & Education model

--- src/main/java/com/oop/academy/models/Education.java
+++ src/main/java/com/oop/academy/models/Education.java
@@ -9,5 +9,51 @@ package com.oop.academy.models;
  * @author asus
  */
 public class Education {
+    private String name;
+    private int attendPeriod;
+    private String degree;
+    private int gpa;
+    private String certificateUrl;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAttendPeriod() {
+        return attendPeriod;
+    }
+
+    public void setAttendPeriod(int attendPeriod) {
+        this.attendPeriod = attendPeriod;
+    }
+
+    public String getDegree() {
+        return degree;
+    }
+
+    public void setDegree(String degree) {
+        this.degree = degree;
+    }
+
+    public int getGpa() {
+        return gpa;
+    }
+
+    public void setGpa(int gpa) {
+        this.gpa = gpa;
+    }
+
+    public String getCertificateUrl() {
+        return certificateUrl;
+    }
+
+    public void setCertificateUrl(String certificateUrl) {
+        this.certificateUrl = certificateUrl;
+    }
+    
     
 }
